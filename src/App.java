846d2f15286637850477f9ac31aeded5172e0f73
te22e-Olsson-Anton <anton.olsson3@elev.ga.ntig.se>
2024-01-22 09:16:57 +0100

    Start meny

--- src/App.java
+++ src/App.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+        
+        boolean spelStart = false;
+
+        Scanner tangetbord = new Scanner(System.in);
+
+        System.out.println("1. För att starta spelet");
+        String start = tangetbord.nextLine();
+        
+        if(start.equalsIgnoreCase("1"))
+        {
+            spelStart = true;
+        }
+
+        
+
+        while (spelStart) 
+        {
+            System.out.println("Spelet är startat");
+        }
+
+    
+      }
 }
+
