92123162b4dc31200fc9fb963f4d7f6abb961e47
te22e-Olsson-Anton <anton.olsson3@elev.ga.ntig.se>
2024-02-07 10:34:28 +0100

    la till thread sleep

--- src/App.java
+++ src/App.java
@@ -1,36 +1,33 @@
 import java.util.Random;
 import java.util.Scanner;
 import java.util.random.*;
-public class App
-{
+
+public class App {
 
     public static void main(String[] args) throws Exception {
-        
+
         boolean gameStart = false;
         boolean enemy = false;
 
-        int playerHP = 100;
-        int enemeyHP = 100;
+        int playerHP = 50;
+        int enemeyHP = 50;
 
         Scanner tb = new Scanner(System.in);
         Random random = new Random();
 
         System.out.println("---------------------------");
         System.out.println("1. För att starta spelet");
-        System.out.println("2. Inställningar");
-        System.out.println("3. Regler");
+        System.out.println("2. Regler");
         System.out.println("---------------------------");
 
         String start = tb.nextLine();
-        
-        if(start.equalsIgnoreCase("1"))
-        {
+
+        if (start.equalsIgnoreCase("1")) {
             gameStart = true;
-        }
-        else if(start.equalsIgnoreCase("2"))
-        {
+        } else if (start.equalsIgnoreCase("2")) {
 
         }
+        
 
         String[][] map = new String[5][5];
 
@@ -67,134 +64,115 @@ public class App
         map[4][3] = "En mur";
         map[4][4] = "En mur";
 
-        while (gameStart) 
-        {
-            
-        System.out.println("Nuvarande Plats " +map[playerRow][playerCol]);
+        while (gameStart) {
 
+            System.out.println("Nuvarande Plats " + map[playerRow][playerCol]);
 
-        System.out.println("Tryck på W för att röra dig uppåt");
-        System.out.println("Tryck på A för att röra dig nedåt");
-        System.out.println("Tryck på S för att röra dig åt höger");
-        System.out.println("Tryck på D för att röra dig åt vänster");
+            System.out.println("Tryck på W för att röra dig uppåt");
+            System.out.println("Tryck på A för att röra dig nedåt");
+            System.out.println("Tryck på S för att röra dig åt höger");
+            System.out.println("Tryck på D för att röra dig åt vänster");
 
-        String input = tb.nextLine();
+            String input = tb.nextLine();
 
-        if(input.equalsIgnoreCase("W"))
-        {
-            playerRow--;
-        }
-        else if(input.equalsIgnoreCase("A"))
-        {
-            playerCol--;
-        }
-        else if(input.equalsIgnoreCase("S"))
-        {
-            playerRow++;
-        }
-        else if(input.equalsIgnoreCase("D"))
-        {
-            playerCol++;
-        }
-        else if(input.equalsIgnoreCase("Q"))
-        {
-            gameStart = false;
-        }
-        
+            if (input.equalsIgnoreCase("W")) {
+                playerRow--;
+            } else if (input.equalsIgnoreCase("A")) {
+                playerCol--;
+            } else if (input.equalsIgnoreCase("S")) {
+                playerRow++;
+            } else if (input.equalsIgnoreCase("D")) {
+                playerCol++;
+            } else if (input.equalsIgnoreCase("Q")) {
+                gameStart = false;
+            }
 
+            if (playerRow == -1) {
+                playerRow++;
+            }
+            if (playerRow == 5) {
+                playerRow--;
+            }
+            if (playerCol == -1) {
+                playerCol++;
+            }
+            if (playerCol == 5) {
+                playerCol--;
+            }
 
-        if(playerRow==-1)
-        {
-            playerRow++;
-        }
-        if(playerRow==5)
-        {
-            playerRow--;
-        }
-        if(playerCol==-1)
-        {
-            playerCol++;
-        }
-        if(playerCol==5)
-        {
-            playerCol--;
-        }
+            if (playerRow == 3 && playerCol == 4) {
+                System.out.println("EN FIENDE");
+                enemy = true;
 
+                while (enemy) {
+                    System.out.println("Din tur att slå");
+                    System.out.println("Tryck space för att slå");
 
-        if(playerRow==3 && playerCol == 4)
-        {
-            System.out.println("EN FIENDE");
-            enemy = true;
-            
+                    String fight = tb.nextLine();
 
-           while (enemy) 
-           {
-                System.out.println("Din tur att slå");
-                System.out.println("Tryck space för att slå");
+                    if (fight.equals(" ")) {
+                        int randomNumberFight1 = random.nextInt(15);
 
-                String fight = tb.nextLine();
+                        System.out.println(randomNumberFight1);
+                        Thread.sleep(1000);
+                        if (randomNumberFight1 > 5) {
 
-                if(fight.equals(" "))
-                {
-                    int randomNumberFight1 = random.nextInt(10);
-                    
-                    System.out.println(randomNumberFight1);
-                    Thread.sleep(1000);
-                    if(randomNumberFight1 > 5)
-                    {
-                        System.out.println("Du lyckades slå fienden!");
-                        int randomNumberDamage1 = random.nextInt(15);
+                            System.out.println("Du lyckades slå fienden!");
+                            Thread.sleep(1000);
+                            int randomNumberDamage1 = random.nextInt(15);
 
-                        enemeyHP -= randomNumberDamage1;
+                            enemeyHP -= randomNumberDamage1;
 
-                        System.out.println("Du slog fienden med " +randomNumberDamage1+ " skada");
-                        System.out.println("Fienden har nu " +enemeyHP+ " liv och du har nu" +playerHP+ " liv");
-                    }
-                    else
-                    {
-                        System.out.println("Du missade ditt slag!");
-                        Thread.sleep(5000);
-                    }
+                            System.out.println("Du slog fienden med " + randomNumberDamage1 + " skada");
+                            Thread.sleep(2000);
+                            System.out.println("Fienden har nu " + enemeyHP + " liv och du har nu" + playerHP + " liv");
+                            Thread.sleep(2000);
+                        } else 
+                        {
+                            System.out.println("Du missade ditt slag!");
+                            
+                        }
 
-                    System.out.println("Fiendens tur att slå");
-                    int randomNumberFight2 = random.nextInt(10);
+                        System.out.println("Fiendens tur att slå");
+                        Thread.sleep(1000);
+                        int randomNumberFight2 = random.nextInt(15);
 
-                    if(randomNumberFight2 >5)
-                    {
+                        if (randomNumberFight2 > 5) {
+                            
+                            System.out.println("Fienden lyckades slå dig");
+                            Thread.sleep(1000);
+                            int randomNumberDamage2 = random.nextInt(15);
 
-                        System.out.println("Fienden lyckades slå dig");
-                        int randomNumberDamage2 = random.nextInt(15);
+                            playerHP -= randomNumberDamage2;
 
-                        playerHP -= randomNumberDamage2;
+                            System.out.println("Fienden slog dig med " + randomNumberDamage2 + " skada");
+                            Thread.sleep(2000);
+                            System.out.println("Du har nu " + playerHP + "liv kvar och fienden har nu" + enemeyHP + "liv");
+                            Thread.sleep(2000);
+                        } else 
+                        {
+                            System.out.println("Fienden missade sitt slag");
+                        }
 
-                        System.out.println("Fienden slog dig med " +randomNumberDamage2+ " skada");
-                        System.out.println("Du har nu " +playerHP+ "liv kvar och fienden har nu" +enemeyHP+ "liv");
-                    }
-                    else
-                    {
-                        System.out.println("Fienden missade sitt slag");
+                        if (playerHP <= 0) 
+                        {
+                            System.out.println("du har förlorat");
+                            enemy = false;
+                        }
+                        if (enemeyHP <= 0) 
+                        {
+                            System.out.println("Du vann");
+                            enemy = false;
+                        }
+
                     }
 
-                    
-
-                    
                 }
-                
+            }
 
-                
-                
-                
-           }
         }
 
-
-        
-
-
- 
     }
+}
 
-    
-        }
-    }
 
